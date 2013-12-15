88b3454bd5ba49193eb855241c32491e773c844f
Yutaka Ogasawara <y.ogagaga@gmail.com>
2013-12-15 21:15:49 +0900

    Add form sample

--- myscript.js
+++ myscript.js
@@ -1,19 +1,22 @@
 var mainCtrl = function($scope) {
-  $scope.users = [
-    {"name":"taguchi", "score":52.22},
-    {"name":"tanaka", "score":60.22},
-    {"name":"yamada", "score":80.22},
-    {"name":"hayashi", "score":5.25},
-    {"name":"tanahashi", "score":75.15},
-    {"name":"yasuda", "score":55.85},
-    {"name":"minami", "score":32.8},
-    {"name":"yanagi", "score":72.2}
-  ];
-  $scope.today = new Date();
+  $scope.addUser = function() {
+    // 実際の処理をここに書く
+  }
+  // $scope.users = [
+  //   {"name":"taguchi", "score":52.22},
+  //   {"name":"tanaka", "score":60.22},
+  //   {"name":"yamada", "score":80.22},
+  //   {"name":"hayashi", "score":5.25},
+  //   {"name":"tanahashi", "score":75.15},
+  //   {"name":"yasuda", "score":55.85},
+  //   {"name":"minami", "score":32.8},
+  //   {"name":"yanagi", "score":72.2}
+  // ];
+  // $scope.today = new Date();
 }
 
-var userItemCtrl = function($scope) {
-  $scope.increment = function() {
-    $scope.user.score++;
-  }
-}
+// var userItemCtrl = function($scope) {
+//   $scope.increment = function() {
+//     $scope.user.score++;
+//   }
+// }
