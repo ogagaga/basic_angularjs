6a08dd785e5f45393f228d226e5c8a4cf2a17e50
Yutaka Ogasawara <y.ogagaga@gmail.com>
2013-12-15 19:48:45 +0900

    Add useful filter that can be used in ng-repeat

--- myscript.js
+++ myscript.js
@@ -2,7 +2,12 @@ var mainCtrl = function($scope) {
   $scope.users = [
     {"name":"taguchi", "score":52.22},
     {"name":"tanaka", "score":60.22},
-    {"name":"yamada", "score":80.22}
+    {"name":"yamada", "score":80.22},
+    {"name":"hayashi", "score":5.25},
+    {"name":"tanahashi", "score":75.15},
+    {"name":"yasuda", "score":55.85},
+    {"name":"minami", "score":32.8},
+    {"name":"yanagi", "score":72.2}
   ];
   $scope.today = new Date();
 }
